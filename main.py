ca95aef468d568aba19b6d38bf5d4f2bdd16dbaa
Ido Shamun <idoesh1@gmail.com>
2020-12-28 22:26:42 +0200

    feat: extract keywords from post content

---
+++ main.py
@@ -0,0 +1,45 @@
+import base64
+import json
+import os
+import nltk
+from newspaper import Article, ArticleException
+from retry import retry
+
+from google.cloud import pubsub_v1
+
+nltk.download('punkt')
+
+# Instantiates a Pub/Sub client
+publisher = pubsub_v1.PublisherClient()
+PROJECT_ID = os.getenv('GOOGLE_CLOUD_PROJECT')
+
+
+# Publishes a message to a Cloud Pub/Sub topic.
+def publish(topic_name, data):
+    # References an existing topic
+    topic_path = publisher.topic_path(PROJECT_ID, topic_name)
+
+    message_json = json.dumps(data)
+    message_bytes = message_json.encode('utf-8')
+
+    # Publishes a message
+    publish_future = publisher.publish(topic_path, data=message_bytes)
+    publish_future.result()  # Verify the publish succeeded
+
+
+@retry(ArticleException, tries=5, delay=1, backoff=2, max_delay=10)
+def download_article(url):
+    article = Article(url)
+    article.download()
+    return article
+
+
+def post_scraper(event, _):
+    if 'data' in event:
+        data = json.loads(base64.b64decode(event['data']).decode('utf-8'))
+        print(f'[{data["id"]}] scraping {data["url"]}')
+        article = download_article(data['url'])
+        article.parse()
+        article.nlp()
+        data['keywords'] = article.keywords
+        publish('post-keywords-extracted', data)
