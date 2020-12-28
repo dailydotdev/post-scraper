132686e328fa585275104b82a74255ecdec61aff
Ido Shamun <idoesh1@gmail.com>
2020-12-28 23:11:08 +0200

    test: add two tests to cover core functionality

---
+++ tests/test_main.py
@@ -0,0 +1,25 @@
+import json
+import base64
+from newspaper import ArticleException
+from main import post_scraper
+
+
+def test_post_scraper(mocker):
+    payload = {'id': '1', 'url': 'https://daily.dev/posts/docker-compose-the-perfect-development-environment',
+               'publicationId': 'pub'}
+    event = {'data': base64.b64encode(json.dumps(payload).encode('utf-8'))}
+    publish = mocker.patch('main.publish')
+    post_scraper(event, None)
+    payload['keywords'] = mocker.ANY
+    publish.assert_called_once_with('post-keywords-extracted', payload)
+
+
+def test_post_scraper_not_available(mocker):
+    payload = {'id': '1', 'url': 'https://daily2.dev', 'publicationId': 'pub'}
+    event = {'data': base64.b64encode(json.dumps(payload).encode('utf-8'))}
+    publish = mocker.patch('main.publish')
+    try:
+        post_scraper(event, None)
+    except ArticleException:
+        pass
+    assert not publish.called, 'method should not have been called'
